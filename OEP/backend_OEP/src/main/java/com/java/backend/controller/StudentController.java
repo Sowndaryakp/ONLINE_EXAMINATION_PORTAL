e64fe82515efd376727c46353e8e6a554d0e36a5
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-05-09 16:40:19 +0530

    authentication completed

--- OEP/backend_OEP/src/main/java/com/java/backend/controller/StudentController.java
+++ OEP/backend_OEP/src/main/java/com/java/backend/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.java.backend.controller;
 
+import com.java.backend.model.AuthResponse;
 import com.java.backend.model.Student;
 import com.java.backend.service.StudentService;
+import com.java.backend.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
     private StudentService studentService;
 
     @PostMapping("/register")
@@ -22,13 +26,35 @@ public class StudentController {
         return studentService.registerStudent(student);
     }
 
+//    @PostMapping("/login")
+//    public String loginStudent(@RequestBody Student student) {
+//        Student authenticatedStudent = studentService.loginStudent(student.getEmail(), student.getPassword());
+//        if (authenticatedStudent != null) {
+//            return "Login successful";
+//        } else {
+//            return "Invalid email or password";
+//        }
+//    }
+
     @PostMapping("/login")
-    public String loginStudent(@RequestBody Student student) {
+    public ResponseEntity<?> loginStudent(@RequestBody Student student) {
         Student authenticatedStudent = studentService.loginStudent(student.getEmail(), student.getPassword());
         if (authenticatedStudent != null) {
-            return "Login successful";
+            String token = jwtTokenUtil.generateToken(authenticatedStudent);
+            boolean approved = authenticatedStudent.isApproved();
+            AuthResponse response = new AuthResponse("Login successful", token, approved); // Create AuthResponse object
+            return ResponseEntity.ok(response); // Return response entity with AuthResponse object
         } else {
-            return "Invalid email or password";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
         }
     }
+
+    // Method to generate token for the authenticated student
+    private String generateTokenForStudent(Student student) {
+        // Generate token using your token generation logic (e.g., using JwtTokenUtil)
+        String token = jwtTokenUtil.generateToken(student);
+        return token;
+    }
+
+
 }
